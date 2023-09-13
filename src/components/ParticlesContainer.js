b99c89d4b5828a6f9ec7a6bec1f83837fbe9b643
Soumyank007 <soumyank.padhy@gmail.com>
2023-09-13 07:07:13 +0530

    Added header and basic homepage layout

---
+++ src/components/ParticlesContainer.js
@@ -0,0 +1,96 @@
+import React, { useCallback } from 'react'
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+
+
+const ParticlesContainer = () => {
+    const particlesInit = useCallback(async (engine) => {
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async () => {}, [])
+
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            className='h-full w-full'
+            options={{
+                background: {
+                    color: {
+                        value: '',
+                    },
+                },
+                fullScreen: { enable: false },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: false,
+                            mode: 'push',
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: 'repulse',
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 90,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: '#000000',
+                    },
+                    links: {
+                        color: '#000000',
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    collisions:{
+                        enable:true
+                    },
+                    move: {
+                        direction: 'none',
+                        enable: true,
+                        outModes: {
+                            default: 'bounce',
+                        },
+                        random: false,
+                        speed: 1,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: 'circle',
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+    )
+}
+
+export default ParticlesContainer
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
+import NavBar from '@/components/NavBar'
 import '@/styles/globals.css'
+import { Montserrat } from 'next/font/google'
+import Head from 'next/head'
+
+const monserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-mont'
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${monserrat.variable} font-mont`}>
+        <NavBar />
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
