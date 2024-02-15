f33a35714e9a314b867d1a32691f8aae65f8ce03
soumyank007 <soumyank.padhy@gmail.com>
2024-02-16 00:46:44 +0530

    feat:Update experience and skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -21,23 +21,25 @@ const Skills = () => {
       <h2 className='font-bold text-8xl mt-32 w-full text-center'>Skills</h2>
       <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight'>
         <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer' whileHover={{ scale: 1.08 }}>
-          Web
+          App
         </motion.div>
         <Skill name="JavaScript" x='-25vw' y='-14vw'></Skill>
-        <Skill name="PHP" x='0vw' y='-21vw'></Skill>
+        <Skill name="PHP" x='0vw' y='-23vw'></Skill>
         <Skill name="TypeScript" x='25vw' y='-14vw'></Skill>
-        <Skill name="Python" x='-28vw' y='10vw'></Skill>
-        <Skill name="Java" x='28vw' y='10vw'></Skill>
-        <Skill name="HTML" x='-25vw' y='-2vw'></Skill>
-        <Skill name="CSS" x='-10vw' y='-15vw'></Skill>
-        <Skill name="VueJS" x='-15vw' y='14vw'></Skill>
-        <Skill name="NuxtJS" x='24vw' y='4vw'></Skill>
-        <Skill name="ReactJS" x='0vw' y='-17vw'></Skill>
+        <Skill name="Python" x='-30vw' y='10vw'></Skill>
+        <Skill name="Java" x='30vw' y='10vw'></Skill>
+        <Skill name="HTML" x='-23vw' y='-2vw'></Skill>
+        <Skill name="CSS" x='-10vw' y='-14vw'></Skill>
+        <Skill name="VueJS" x='-18vw' y='14vw'></Skill>
+        <Skill name="NuxtJS" x='28vw' y='4vw'></Skill>
+        <Skill name="ReactJS" x='0vw' y='-19vw'></Skill>
+        <Skill name="React Native" x='22vw' y='-3vw'></Skill>
         <Skill name="Node JS" x='10vw' y='-10vw'></Skill>
+        <Skill name="SpringBoot" x='14vw' y='6vw'></Skill>
         <Skill name="MongoDb" x='-17vw' y='5vw'></Skill>
-        <Skill name="SQL" x='12vw' y='10vw'></Skill>
-        <Skill name="NextJS" x='0vw' y='17vw'></Skill>
-        <Skill name="Django" x='-12vw' y='-10vw'></Skill>
+        <Skill name="SQL" x='0vw' y='11vw'></Skill>
+        <Skill name="NextJS" x='0vw' y='19vw'></Skill>
+        <Skill name="Django" x='-10vw' y='-10vw'></Skill>
         <Skill name="Docker" x='-12vw' y='0vw'></Skill>
       </div>
     </>
