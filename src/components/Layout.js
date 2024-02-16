a7db3c5a29e1f5592e53b04dc7ea183965c7d7c6
soumyank007 <soumyank.padhy@gmail.com>
2024-02-16 09:08:41 +0530

    bugfix:fix build issue

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 
 const Layout = ({children,className=''}) => {
