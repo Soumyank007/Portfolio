32a6daf15747a250c4db0d8502e1e711291c1003
soumyank007 <soumyank.padhy@gmail.com>
2024-02-16 08:52:46 +0530

    feat:Add vercel speed insights

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 const Layout = ({children,className=''}) => {
   return (
     <div className={`w-full h-full inline-block z-0 p-32 ${className} dark:bg-dark`}>
         {children}
+        <SpeedInsights />
     </div>
   )
 }
