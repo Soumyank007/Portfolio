94918b3c23adbd11935e55a90c8c30dfe542fbbc
soumyank007 <soumyank.padhy@gmail.com>
2024-02-17 20:05:27 +0530

    style:fix padding

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -93,7 +93,7 @@ const NavBar = () => {
             initial={{ scale: 0, opacity: 0, x: '-50%', y: '-50%' }}
             animate={{ scale: 1, opacity: 1 }}
             transition={{ duration: 0.12 }}
-            className='min-w-[70vw] z-30 flex flex-col justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32'>
+            className='min-w-[90vw] z-30 flex flex-col justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-24'>
             <nav className='select-none flex items-center flex-col justify-center'>
               <CustomMobileLink href="/" title="Home" className='' toggle={handleClick} />
               <CustomMobileLink href="/about" title="About" className='' toggle={handleClick} />
@@ -127,7 +127,7 @@ const NavBar = () => {
                 dispatch(toggleTheme());
                 setMode(mode === 'light' ? 'dark' : 'light');
               }}
-              className={` mt-8 text-xs h-8 flex items-center justify-center rounded-full p-1 ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'
+              className={`mt-8 text-xs h-8 flex items-center justify-center rounded-full p-1 ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'
                 }`}
             >
               {mode === 'dark' ? <SunIcon className={'fill-light h-4 mx-1'} /> : <MoonIcon className={'fill-dark h-4 mx-1'} />}
