60e042d607e1d314345615df64623f89ad2d1e4b
Soumyank007 <soumyank.padhy@gmail.com>
2024-04-01 20:27:33 +0530

    chore:exclude static aritcles path from sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,7 @@ const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
 module.exports = {
     siteUrl:siteUrl,
     generateRobotsTxt: true,
-    exclude: ['/server-sitemap.xml'],
+    exclude: ['/articles/*','/server-sitemap.xml'],
     robotsTxtOptions: {
       additionalSitemaps: [
         `${siteUrl}/server-sitemap.xml`,
